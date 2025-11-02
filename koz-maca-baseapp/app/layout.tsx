3d6223106a4ac916034ce74d19f2b5ef33a0dbae
Yunus Emre ARAS <yemrearas@Yunus-MacBook-Air.local>
2025-11-03 00:25:25 +0300

    fixes

--- koz-maca-baseapp/app/layout.tsx
+++ koz-maca-baseapp/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Inter, Source_Code_Pro } from "next/font/google";
 import { RootProvider } from "./rootProvider";
 import "./globals.css";
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
-
-const sourceCodePro = Source_Code_Pro({
-  variable: "--font-source-code-pro",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: process.env.NEXT_PUBLIC_PROJECT_NAME,
@@ -26,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${sourceCodePro.variable}`}>
+      <body>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
