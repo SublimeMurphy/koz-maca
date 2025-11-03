cc371e909b214d84d4ecdeed2b5fd5719d5b31d2
Yunus Emre ARAS <yemrearas@Yunus-MacBook-Air.local>
2025-11-04 00:49:24 +0300

    fixes

--- koz-maca-baseapp/next.config.ts
+++ koz-maca-baseapp/next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/.well-known/farcaster.json",
+        destination: "/farcaster/manifest.json",
+      },
+    ];
+  },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
